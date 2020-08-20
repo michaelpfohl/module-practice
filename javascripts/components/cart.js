4ee99013218ff5536ce41c2acbf06c1bb76e4928
Michael Pfohl <rmpfohl@gmail.com>
2020-08-20 14:08:34 -0500

    removed redundant cart.js file

--- javascripts/components/cart.js
+++ javascripts/components/cart.js
@@ -1,7 +1,3 @@
-import { setCart, getCart, emptyCart } from './../helpers/data/cartData.js';
-import { cartTotal } from './cartTotal.js';
-import { makeCartBody } from './makeCartBody.js';
-import { makeCartModal, chargeIt, showCartItems } from './makeCartModal.js';
-import { makeCart, addToCart } from './makeAndAddCart.js';
+// import { makeCart, addToCart } from './makeAndAddCart.js';
 
-export { makeCart, addToCart }
+// export { makeCart, addToCart }
--- javascripts/components/store.js
+++ javascripts/components/store.js
@@ -1,4 +1,4 @@
-import { addToCart } from './cart.js';
+import { addToCart } from './makeAndAddCart.js';
 
 const makeStore = (array, titles = false) => {
     $("#store").html("");
